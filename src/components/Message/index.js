88335de03e7f45bf2b7fff10358d9cfeaeb0177b
VernalEquinox <pieroapruzzese@gmail.com>
2021-02-09 23:09:23 +0100

    adding form

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";//modified
 import io from "socket.io-client";
+import {Form,Button} from "react-bootstrap"
 
 const connOpt = {
 	transports: ["websocket"]
 }
 
-let socket = io("https://striveschool-api.herokuapp.com") 
+let socket = io("https://striveschool-api.herokuapp.com", connOpt) 
 
 function Message() {
     const [username, setUsername] = useState(null)
@@ -23,8 +24,22 @@ function Message() {
 
 
 
-    return (<>
-        <div className="Message">
+    return (
+        <>
+            <div className="Message">
+        <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>name</Form.Label>
+                <Form.Control type="text" placeholder="Enter name" />
+                <Form.Label>message</Form.Label>
+                <Form.Control type="text" placeholder="Enter message" />
+                
+                </Form.Group>
+                </Form>
+
+                <Button variant="primary">Primary</Button>{' '}
+        
+        
             <ul id="messages">MESSAGE HERE
                 {messages.map((msg, index) => (<li key={index} className={msg.user === username ? "myMsg" : "message"}>
                     {msg.user} : {msg.message}
